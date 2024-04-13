869f6f6c0ac9ab7426a0647177628bc277d3934f
Zhen <165939197+ZhenNiu6@users.noreply.github.com>
2024-04-12 20:25:06 -0700

    lab2

--- labs/fibvec/FibVec.cpp
+++ labs/fibvec/FibVec.cpp
@@ -5,3 +5,124 @@
 
 
 // FibVec Function Implementations
+FibVec::FibVec(){
+    num = 0;
+    cap = 1;
+    n = 2;
+    fibs = new int[cap];
+}
+
+FibVec::FibVec(const FibVec& other){
+    num = other.num;
+    cap = other.cap;
+    n = other.n;
+    fibs = new int[cap];
+    for(size_t i = 0; i < num; i ++){
+        fibs[i] = other.fibs[i];
+    }
+}
+
+
+
+FibVec::~FibVec(){
+    delete [] fibs;
+}
+
+size_t FibVec::capacity() const{
+    return cap;
+}
+
+size_t FibVec::count() const{
+    return num;
+}
+
+void FibVec::insert(int value, size_t index){
+    if(index >= num){
+        throw std::out_of_range("insert()");
+    }
+    else{
+        if((num+1) == cap){
+            int* temp = new int[fib(n+1)];
+            cap = fib(n+1);
+            n ++;
+            for(size_t i = 0; i < index; i ++){
+                temp[i] = fibs[i];
+            }
+            for(size_t i = index; i < num; i ++){
+                temp[i+1] = fibs[i];
+            }
+            temp[index] = value;
+            delete [] fibs;
+            fibs = temp;
+            count ++;
+        }
+        else{
+            for(size_t i = index; i < num; i ++){
+                fibs[i+1] = fibs[i];
+            }
+            fibs[index] = value;
+            count ++;
+        }
+    }
+}
+
+int FibVec::lookup(size_t index) const{
+    if(index >= num){
+        throw std::out_of_range("lookup()");
+    }
+    return fibs[index];
+}
+
+int FibVec::pop(){
+    if(num == 0){
+        throw std::underflow_error("pop()");
+    }
+    if((num - 1) < fib(n-2)){
+        cap = fib(n-1);
+        n --;
+    }
+    int* temp = new int[cap];
+    for(size_t i = 0; i < num - 1; i ++){
+        temp[i] = fibs[i];
+    }
+    int value = fibs[num];
+    delete [] fibs;
+    fibs = temp;
+    num --;
+    return value;
+}
+
+void FibVec::push(int value){
+    if(num == cap){
+        cap = fib(n+1);
+        n ++;
+    }
+    int* temp = new int[cap];
+    for(size_t i = 0; i < num; i ++){
+        temp[i] = fibs[i];
+    }
+    temp[num] = value;
+}
+
+int FibVec::remove(size_t index){
+    if(index >= num){
+        throw std::out_of_range("remove()");
+    }
+    if((num - 1) < fib(n-2)){
+        cap = fib(n-1);
+        n --;
+    }
+    int* temp = new int[cap];
+    for(size_t i = 0; i < index; i ++){
+        temp[i] = fibs[i];
+    }
+    int value = fibs[index];
+    for(size_t i = index; i < num; i ++){
+        temp[i] = fibs[i+1];
+    }
+    delete [] fibs;
+    fibs = temp;
+    num --;
+    return value;
+
+}
--- labs/fibvec/FibVec.h
+++ labs/fibvec/FibVec.h
@@ -6,14 +6,34 @@
 
 class FibVec {
   // Member Variables
+    size_t num;
+    size_t cap;
+    size_t n;
+    int* fibs;
 
   // Helper Functions
+    size_t fib(n){
+      if(n <= 1){
+        return n;
+      }
+    return fib(n - 1) + fib(n - 2);
+    }
+    
 
 public:
   // Constructor and Destructor
+    FibVec();
+    FibVec(const FibVec& other);
+    ~FibVec();
 
   // Member Functions
-
+    size_t capacity() const;
+    size_t count() const;
+    void insert(int value, size_t index);
+    int lookup(size_t index) const;
+    int pop();
+    void push(int value);
+    int remove(size_t index);
 };
 
 #endif
