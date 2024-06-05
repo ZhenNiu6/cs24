bfaf0cd2bf1422fd085243202a8f2118374f922c
Zhen <165939197+ZhenNiu6@users.noreply.github.com>
2024-06-05 10:35:30 -0700

    lab9

--- labs/voxhop/VoxMap.cpp
+++ labs/voxhop/VoxMap.cpp
@@ -54,17 +54,29 @@ VoxMap::~VoxMap() {}
 
 Route VoxMap::route(Point src, Point dst) {
     // Point floor_s(src.x, src.y, src.z - 1);
-    if((src.z < 0) || (src.y < 0) || (src.x < 0)){
+    if((src.z < 0) || (src.z >= height) || (src.y < 0) || (src.y >= length) || (src.x < 0) || (src.x >= width)){
         throw InvalidPoint(src);
     }
-    if((voxmap[src.z][src.y][src.x]) || (src.z == 0) || (!voxmap[src.z-1][src.y][src.x]) || (src.x >= width) || (src.x < 0) || (src.y >= length) || (src.y < 0) || (src.z >= height) || (src.z < 0)){
+    if((voxmap[src.z][src.y][src.x])){
+        throw InvalidPoint(src);
+    }
+    if(src.z == 0){
+        throw InvalidPoint(src);
+    }
+    if(!voxmap[src.z-1][src.y][src.x]){
         throw InvalidPoint(src);
     }
     // Point floor_d(dst.x, dst.y, dst.z - 1);
-    if((dst.z < 0) || (dst.y < 0) || (dst.x < 0)){
-        throw InvalidPoint(src);
+    if((dst.z < 0) || (dst.y < 0) || (dst.x < 0) || (dst.x >= width) || (dst.y >= length) || (dst.z >= height)){
+        throw InvalidPoint(dst);
     }
-    if((voxmap[dst.z][dst.y][dst.x]) || (dst.z == 0) || (!voxmap[dst.z-1][dst.y][dst.x]) || (dst.x >= width) || (dst.x < 0) || (dst.y >= length) || (dst.y < 0) || (dst.z >= height) || (dst.z < 0)){
+    if((voxmap[dst.z][dst.y][dst.x])){
+        throw InvalidPoint(dst);
+    }
+    if(dst.z == 0){
+        throw InvalidPoint(dst);
+    }
+    if(!voxmap[dst.z-1][dst.y][dst.x]){
         throw InvalidPoint(dst);
     }
     throw NoRoute(src, dst);
